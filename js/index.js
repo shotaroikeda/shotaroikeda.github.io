f2e77948ed0ae83c3a6330677b3e71f6a60dffc3
shotaroikeda <ikeda.shot@gmail.com>
2016-02-15 00:51:58 -0600

    fixed height logic

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 function docFill() {
     if($(document).height() <= $(window).height())
     {
-        $(".restFill").css("height",$(window).height() + ($("body").height() - $(".sidebar-elem").height()));
+	var temp_h = $(window).height() + ($("body").height() - $(".sidebar-elem").height());
+        $(".restFill").css("height", $(document).height() - $(".sidebar-elem").height());
+	console.log(temp_h);
     }
     else
     {
